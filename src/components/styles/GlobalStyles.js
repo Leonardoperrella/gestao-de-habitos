4ae8f76b70c997249b2b697ba2bae91f48ba82e7
JuarezGoesJunior <juarezantoniog12@gmail.com>
2021-03-10 20:49:04 -0300

    delete GlobalStyles

--- src/components/styles/GlobalStyles.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from "styled-components";
-
-const GlobalStyles = createGlobalStyle`
-    :root{
-        font-family: 'Roboto', sans-serif;
-    }
-`;
-
-export default GlobalStyles;
