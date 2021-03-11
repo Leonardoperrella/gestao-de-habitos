db02591e2347631b5127d068fd8d597a1f34c87b
JuarezGoesJunior <juarezantoniog12@gmail.com>
2021-03-11 15:01:07 -0300

    att packed

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,12 +2,15 @@ import Background from "../../Images/BackgroundLoginRegister.jpg";
 import GlobalContainer from "../../components/GlobalContainer";
 import BackGroundImage from "../../components/BackGroundImage";
 import GlobalWrap from "../../components/GlobalWrap";
+import FormLogin from "../../components/FormLogin";
 
 const Login = () => {
   return (
     <GlobalContainer>
       <BackGroundImage image={Background} />
-      <GlobalWrap></GlobalWrap>
+      <GlobalWrap>
+        <FormLogin />
+      </GlobalWrap>
     </GlobalContainer>
   );
 };
