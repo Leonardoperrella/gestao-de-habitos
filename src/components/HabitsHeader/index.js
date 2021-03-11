ed1e94f34e9160420c3631d3a30780e49288187b
Junia Nunes <jununes.bermejo@gmail.com>
2021-03-11 15:58:08 -0300

    Add HabitsHeader components and Habits pages.

---
+++ src/components/HabitsHeader/index.js
@@ -0,0 +1,13 @@
+import { HabitTitle, LinkHabit, LinkAddHabit } from "./style";
+
+const HabitsHeader = () => {
+  return (
+    <div>
+      <HabitTitle>Habits</HabitTitle>
+      <LinkHabit>Habit</LinkHabit>
+      <LinkAddHabit>Add Habit</LinkAddHabit>
+    </div>
+  );
+};
+
+export default HabitsHeader;
