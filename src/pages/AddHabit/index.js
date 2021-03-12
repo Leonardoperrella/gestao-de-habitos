ddf2aa84001695d21eb0c288b1fe9c23d53e7f28
leonardo perrella <leonardo.perrella@yahoo.com.br>
2021-03-12 14:15:21 -0300

    post on add habit and Form

--- src/pages/AddHabit/index.js
+++ src/pages/AddHabit/index.js
@@ -1,26 +1,60 @@
 import { useState } from "react";
-import BackGroundImage from "../../components/BackGroundImage";
-import GlobalContainer from "../../components/GlobalContainer";
-import Background from "../../Images/BackgroundAddHabit.jpg";
-import GlobalWrap from "../../components/GlobalWrap";
-import FormUserInput from "../../components/FormUserInput";
-import { ButtonSubmit, ButtonIcon } from "../../components/FormUser/style";
-import Menu from "../../components/Menu";
-import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import api from "../../services/api";
 import * as yup from "yup";
+import GlobalWrap from "../../components/GlobalWrap";
+import GlobalContainer from "../../components/GlobalContainer";
+import BackGroundImage from "../../components/BackGroundImage";
+import FormUserInput from "../../components/FormUserInput";
+import Background from "../../Images/BackgroundAddHabit.jpg";
+import Menu from "../../components/Menu";
+import FormActionSelect from "../../components/FormActionSelect";
+import FormAction from "../../components/FormAction";
 
 const AddHabit = () => {
   const [habitError, setHabitError] = useState({});
+
+  const [token] = useState(() => {
+    const sessionToken = localStorage.getItem("token") || "";
+    return JSON.parse(sessionToken);
+  });
+  const [user_id] = useState(() => {
+    const sessionId = localStorage.getItem("user_id") || "";
+    return JSON.parse(sessionId);
+  });
+
+  const category = [
+    { value: "Aim", content: "Aim" },
+    { value: "Mechanical", content: "Mechanical" },
+    { value: "Decision Making", content: "Decision Making" },
+    { value: "Game Sense", content: "Game Sense" },
+  ];
+  const difficulty = [
+    { value: "Easy", content: "Easy" },
+    { value: "Medium", content: "Medium" },
+    { value: "Hard", content: "Hard" },
+  ];
+
+  const frequency = [
+    { value: "Daily", content: "Daily" },
+    { value: "Weekly", content: "Medium" },
+    { value: "Monthly", content: "Hard" },
+  ];
+
+  const how_much_achieved = [
+    { value: 25, content: "25%" },
+    { value: 50, content: "50%" },
+    { value: 75, content: "75%" },
+    { value: 100, content: "100%" },
+  ];
+
   const schema = yup.object().shape({
     title: yup.string().required("Field Required"),
-    // category: yup.string().required("Field Required"),
-    // difficulty: yup.string().required("Field Required"),
-    // frequency: yup.string().required("Field Required"),
-    // achieved: yup.string().required("Field Required"),
-    // how_much_achieved: yup.string().required("Field Required"),
-    // user: yup.string().required("Field Required"),
+    category: yup.string().required("Field Required"),
+    difficulty: yup.string().required("Field Required"),
+    frequency: yup.string().required("Field Required"),
+    how_much_achieved: yup.number().required("Field Required"),
   });
 
   const { register, handleSubmit, errors, reset } = useForm({
@@ -28,11 +62,13 @@ const AddHabit = () => {
   });
 
   const handleForm = (data) => {
-    console.log(data);
-    return;
+    data = { ...data, user: user_id, achieved: false };
     api
-      .post("/habits/", data)
+      .post("/habits/", data, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
       .then((response) => {
+        console.log(response);
         reset();
       })
       .catch((e) => setHabitError(e.response));
@@ -42,14 +78,39 @@ const AddHabit = () => {
     <GlobalContainer>
       <BackGroundImage image={Background} />
       <GlobalWrap>
-        <form onSubmit={handleSubmit(handleForm)}>
+        <FormAction handleSubmit={handleSubmit(handleForm)} title="Add Habit">
           <FormUserInput name="title" inputRef={register} error={errors.title}>
             Title
           </FormUserInput>
-          <ButtonSubmit>
-            <ButtonIcon />
-          </ButtonSubmit>
-        </form>
+          <FormActionSelect
+            name="category"
+            inputRef={register}
+            error={errors.category}
+          >
+            {category}
+          </FormActionSelect>
+          <FormActionSelect
+            name="difficulty"
+            inputRef={register}
+            error={errors.difficulty}
+          >
+            {difficulty}
+          </FormActionSelect>
+          <FormActionSelect
+            name="frequency"
+            inputRef={register}
+            error={errors.frequency}
+          >
+            {frequency}
+          </FormActionSelect>
+          <FormActionSelect
+            name="how_much_achieved"
+            inputRef={register}
+            error={errors.how_much_achieved}
+          >
+            {how_much_achieved}
+          </FormActionSelect>
+        </FormAction>
         <Menu />
       </GlobalWrap>
     </GlobalContainer>
