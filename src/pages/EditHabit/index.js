03ef41e2e1ca8d08ba8b6adc2581612aa541880a
Junia Nunes <jununes.bermejo@gmail.com>
2021-03-12 13:30:49 -0300

    Add EditHabit page.

---
+++ src/pages/EditHabit/index.js
@@ -0,0 +1,13 @@
+import GlobalContainer from "../../components/GlobalContainer";
+import GlobalWrap from "../../components/GlobalWrap";
+import Menu from "../../components/Menu";
+
+const EditHabit = () => {
+  return (
+    <GlobalContainer>
+      <GlobalWrap></GlobalWrap>
+      <Menu></Menu>
+    </GlobalContainer>
+  );
+};
+export default EditHabit;
