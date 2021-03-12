6dfae7ffed986ad9f03830bfc7e471b570b9de97
JuarezGoesJunior <juarezantoniog12@gmail.com>
2021-03-12 19:34:59 -0300

    refactory button

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -24,7 +24,7 @@ const Menu = () => {
         <MenuIcon edge="end">
           <Person />
         </MenuIcon>
-        <MenuIcon color="black">
+        <MenuIcon>
           <Search />
         </MenuIcon>
       </MenuIconsWrap>
