2b9039ec66adef4ebab65cc7a5d114544b9c9041
JuarezGoesJunior <juarezantoniog12@gmail.com>
2021-03-12 14:36:38 -0300

    refactory select

--- src/components/FormActionSelect/style.js
+++ src/components/FormActionSelect/style.js
@@ -12,8 +12,8 @@ export const Select = styled.select`
 
   font-size: 1rem;
   padding-left: 0.5rem;
-  background-color: #f9f9f9;
-  border: 2px solid #e8f0fe;
+  background-color: #e8f0fe;
+  border: 2px solid #b7bfcc;
   padding-top: 0.3rem;
 
   -webkit-appearance: none;
