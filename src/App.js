e594c780b594169603c64550536b4d3b000fef0e
JuarezGoesJunior <juarezantoniog12@gmail.com>
2021-03-16 19:29:27 -0300

    refactory UserProvider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import Routes from "./routes";
 import GlobalStyles from "./components/GlobalStyle";
-import UserProvider from "./providers/UserProvider";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <UserProvider>
-        <Routes />
-      </UserProvider>
+      <Routes />
     </>
   );
 }
