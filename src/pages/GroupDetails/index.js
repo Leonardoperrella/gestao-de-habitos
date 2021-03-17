3afc53cb875a54a4756f91a037e14c879fa81148
JuarezGoesJunior <juarezantoniog12@gmail.com>
2021-03-17 12:14:07 -0300

    att GroupDetails

--- src/pages/GroupDetails/index.js
+++ src/pages/GroupDetails/index.js
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CardActivite from "../../components/CardActivite";
+import CardGoals from "../../components/CardGoals";
 import GlobalContainer from "../../components/GlobalContainer";
 import GlobalWrap from "../../components/GlobalWrap";
 import Menu from "../../components/Menu";
 import api from "../../services/api";
-import { SubTitle } from "./style";
+
+import { ButtonTopic, ContainerShow, SetaDrop } from "./style";
+
+import { ReactComponent as SetaSvg } from "../../svgs/seta-suspensa.svg";
 
 const GroupDetails = () => {
-  const state = useParams();
-
   const [group, setGroup] = useState({});
   const [showGroup, setShowGroup] = useState(false);
+  const [showGoals, setShowGoals] = useState(true);
+  const [showActivies, setShowActivies] = useState(true);
 
   const { id } = useParams();
   console.log(id);
@@ -28,6 +32,15 @@ const GroupDetails = () => {
     getGroupActivities();
   }, [id]);
 
+  const handleShowGoals = () => {
+    setShowGoals(!showGoals);
+  };
+
+  const handleShowActivies = () => {
+    console.log("oi");
+    setShowActivies(!showActivies);
+  };
+
   return (
     <GlobalContainer>
       <GlobalWrap>
@@ -35,19 +48,37 @@ const GroupDetails = () => {
           <>
             <h1>{group.name}</h1>
 
-            <SubTitle>Goals</SubTitle>
-            {group.goals.map(
-              ({ id, title, difficulty, achieved, how_much_achieved }) => (
-                <div key={id}>
-                  <h1>{title}</h1>
-                </div>
-              )
+            <ButtonTopic onClick={handleShowGoals} showItem={showGoals}>
+              Goals
+              <SetaSvg />
+            </ButtonTopic>
+            {showGoals && (
+              <ContainerShow>
+                {group.goals.map(
+                  ({ id, title, difficulty, achieved, how_much_achieved }) => (
+                    <CardGoals
+                      key={id}
+                      title={title}
+                      difficulty={difficulty}
+                      how_much_achieved={how_much_achieved}
+                      achieved={achieved}
+                    />
+                  )
+                )}
+              </ContainerShow>
             )}
 
-            <SubTitle>Activities</SubTitle>
-            {group.activities.map(({ id, title, realization_time }) => (
-              <CardActivite key={id} id={id} title={title} />
-            ))}
+            <ButtonTopic onClick={handleShowActivies} showItem={showActivies}>
+              Activities
+              <SetaSvg />
+            </ButtonTopic>
+            {showActivies && (
+              <ContainerShow>
+                {group.activities.map(({ id, title, realization_time }) => (
+                  <CardActivite key={id} id={id} title={title} />
+                ))}
+              </ContainerShow>
+            )}
           </>
         ) : (
           "Grupo não encontrado"
--- src/pages/GroupDetails/style.js
+++ src/pages/GroupDetails/style.js
@@ -1,5 +1,43 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-export const SubTitle = styled.h2`
-  font-size: 1.5rem;
+export const ButtonTopic = styled.button`
+  font-size: 2rem;
+  font-family: Tungsten;
+  color: #252525;
+  letter-spacing: 2px;
+  margin: 1rem 0 0.5rem 0;
+  width: 300px;
+  position: relative;
+  background-color: #fff;
+  border-radius: 5px;
+  border: 1px solid #252525;
+
+  svg {
+    height: 1rem;
+    width: 1rem;
+    position: absolute;
+    right: 1rem;
+    top: calc(50% - 0.5rem);
+    transition: 0.3s;
+
+    transform: ${({ showItem }) =>
+      showItem ? "rotate(0)" : "rotate(180deg)"};
+  }
+`;
+
+const animationShow = keyframes`
+  from{
+    opacity:0;
+    transform: translatey(-30px);
+  }
+  to{
+    opacity:initial;
+    transform: initial;
+  }
+`;
+
+export const ContainerShow = styled.div`
+  div {
+    animation: ${animationShow} 0.3s forwards;
+  }
 `;
